c2bd0a77da61faa985c943ca1341c4628e266c4a
pvq2k2 <vanquyet2392002@gmail.com>
2022-01-11 18:02:25 +0700

    feat: Create UI - Create UI Sign up, Sign In, dashboard, newsAdd - Router to it

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Footer from "./components/footer";
 import AboutPage from "./page/about";
 import HomePage from "./page/home";
 import NewsPage from "./page/news";
+import SignInPage from "./page/signIn";
+import SignUpPage from "./page/signUp";
+import Dashboard from "./page/admin/dashboard";
+import NewsAd from "./page/admin/news";
+import NewsAdd from "./page/admin/newsAdd";
 
 const router = new Navigo("/", { linksSelector: "a" });
 const render = (content) => {
@@ -21,6 +26,24 @@ router.on({
     },
     "/news": () => {
         render(NewsPage.print());
+    },
+    "/signup": () => {
+        render(SignUpPage.print());
+    },
+    "/signin": () => {
+        render(SignInPage.print());
+    },
+    "/admin/dashboard": () => {
+        render(Dashboard.print());
+    },
+    "/admin/news": () => {
+        render(NewsAd.print());
+    },
+    "/admin/news/add": () => {
+        render(NewsAdd.print())
+    },
+    "/admin/news/:id/edit": () => {
+
     }
 });
 // router.on("/about", function() {
